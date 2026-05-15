0281d072bce14fd4f516c8ca8682a4b9aaccce0e
f1047 <14920359+f1047@users.noreply.github.com>
2026-05-15 23:15:10 +0900

    biome and astro check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://f1047.github.io',
+	site: "https://f1047.github.io",
 });
